becfc7c37a6b26702b389261a028709519265ad4
Evgeny GooDi Bunakov <goodibunakov@gmail.com>
2018-03-29 14:02:25 +0700

    добавлен сплэш скрин и иконка приложения

---
+++ app/src/main/java/ru/goodibunakov/testforzennex2018/SplashActivity.java
@@ -0,0 +1,41 @@
+package ru.goodibunakov.testforzennex2018;
+
+import android.content.Intent;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class SplashActivity extends AwesomeSplash {
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        /* you don't have to override every property */
+
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(1000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.zennex_logo); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(1500); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.FadeIn); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Title
+        configSplash.setTitleSplash(getString(R.string.splash_text));
+        configSplash.setTitleTextColor(android.R.color.white);
+        configSplash.setTitleTextSize(10f); //float value
+        configSplash.setAnimTitleDuration(600);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+    }
+
+    @Override
+    public void animationsFinished() {
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+    }
+}
