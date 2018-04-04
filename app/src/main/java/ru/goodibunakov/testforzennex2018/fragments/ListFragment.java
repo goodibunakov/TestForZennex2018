a6bee8300f10fac29a26e6fbc713d7cfc496a496
Evgeny GooDi Bunakov <goodibunakov@gmail.com>
2018-04-04 14:36:39 +0700

    сделал обработку нажатия кнопки назад (во время диалога добавления человека)

--- app/src/main/java/ru/goodibunakov/testforzennex2018/fragments/ListFragment.java
+++ app/src/main/java/ru/goodibunakov/testforzennex2018/fragments/ListFragment.java
@@ -10,16 +10,12 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
-import java.util.List;
-
 import ru.goodibunakov.testforzennex2018.R;
-import ru.goodibunakov.testforzennex2018.activities.MainActivity;
 import ru.goodibunakov.testforzennex2018.adapters.PersonAdapter;
 import ru.goodibunakov.testforzennex2018.model.Person;
 import ru.goodibunakov.testforzennex2018.utils.DatabaseHelper;
@@ -72,44 +68,75 @@ public class ListFragment extends Fragment {
         });
 
         fab.setOnClickListener(new View.OnClickListener() {
-                                   @Override
-                                   public void onClick(View view) {
-                                       AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                                       builder.setTitle(getResources().getString(R.string.dialog_add_title));
-                                       View addLayout = getActivity().getLayoutInflater().inflate(R.layout.add_layout, null);
-                                       final EditText edText = addLayout.findViewById(R.id.edit_text);
-                                       builder.setView(addLayout);
-                                       //кнопка добавить
-                                       builder.setPositiveButton(R.string.btn_add, new DialogInterface.OnClickListener() {
-                                           @Override
-                                           public void onClick(DialogInterface dialog, int which) {
-                                               String newPersonName = "";
-                                               if (edText.getText() != null) newPersonName = edText.getText().toString();
-                                               Person person = new Person(newPersonName, 0);
-                                               databaseHelper.saveNewPerson(person);
-                                               adapter.updateCheckStateList();
-                                               adapter.notifyItemInserted(adapter.getItemCount());
-                                               rv.scrollToPosition(adapter.getItemCount()-1);
-                                           }
-                                       });
-                                       //кнопка отменить
-                                       builder.setNegativeButton(R.string.btn_rev, new DialogInterface.OnClickListener() {
-                                           @Override
-                                           public void onClick(DialogInterface dialog, int which) {
-                                               //если что-то ввели и нажали Назад
-                                               dialog.dismiss();
-                                           }
-                                       });
-                                       builder.create().show();
-                                   }
-                               });
-
-                rv.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(getResources().getString(R.string.dialog_add_title));
+                View addLayout = getActivity().getLayoutInflater().inflate(R.layout.add_layout, null);
+                final EditText edText = addLayout.findViewById(R.id.edit_text);
+                builder.setView(addLayout);
+                //кнопка добавить
+                builder.setPositiveButton(R.string.btn_add, new DialogInterface.OnClickListener() {
                     @Override
-                    public boolean onLongClick(View view) {
-                        return false;
+                    public void onClick(DialogInterface dialog, int which) {
+                        String newPersonName = "";
+                        if (edText.getText() != null) newPersonName = edText.getText().toString();
+                        Person person = new Person(newPersonName, 0);
+                        databaseHelper.saveNewPerson(person);
+                        adapter.updateCheckStateList();
+                        adapter.notifyItemInserted(adapter.getItemCount());
+                        rv.scrollToPosition(adapter.getItemCount() - 1);
                     }
                 });
+                //кнопка отменить
+                builder.setNegativeButton(R.string.btn_rev, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //если что-то ввели и нажали Назад
+                        dialog.dismiss();
+                    }
+                });
+                builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialogInterface) {
+                        if (!edText.getText().toString().isEmpty()) {
+                            AlertDialog.Builder builderBack = new AlertDialog.Builder(getActivity());
+                            builderBack.setTitle(getResources().getString(R.string.dialog_backbutton_title));
+                            builderBack.setPositiveButton(getResources().getString(R.string.btn_save), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    String newPersonName = "";
+                                    if (edText.getText() != null)
+                                        newPersonName = edText.getText().toString();
+                                    Person person = new Person(newPersonName, 0);
+                                    databaseHelper.saveNewPerson(person);
+                                    adapter.updateCheckStateList();
+                                    adapter.notifyItemInserted(adapter.getItemCount());
+                                    rv.scrollToPosition(adapter.getItemCount() - 1);
+                                }
+                            });
+                            builderBack.setNegativeButton(getResources().getString(R.string.btn_not), new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            });
+                            builderBack.create().show();
+                        }
+                    }
+                });
+                builder.create().show();
+            }
+        });
+
+        rv.setOnLongClickListener(new View.OnLongClickListener()
+
+        {
+            @Override
+            public boolean onLongClick(View view) {
+                return false;
+            }
+        });
         return v;
 
     }
